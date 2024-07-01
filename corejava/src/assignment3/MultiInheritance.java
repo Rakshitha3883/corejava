596ee69b6d2dfcb27b1f82141486791b092e0d8f
DELL <DELL@DESKTOP-5788940>
2024-07-01 14:19:27 +0530

    first commit

---
+++ corejava/src/assignment3/MultiInheritance.java
@@ -0,0 +1,12 @@
+package assignment3;
+
+public class MultiInheritance {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		State st = new State("Nile","Statue of liberty","Himalayas","Sambar","Taj Mahal","kanchajunga","chilika","Sundarbans");
+		System.out.println("Multiple Inheritance");
+		System.out.println(st);
+	}
+
+
+}
